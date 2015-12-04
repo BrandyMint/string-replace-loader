3073d6f48c1730a5218b2ae8eedc22a0a7acdccd
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-12-04 15:30:05 +0300

    Add support for multiple field

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ module.exports = function (source) {
   this.cacheable();
   var query = utils.parseQuery(this.query);
 
-  if (typeof query.search !== 'undefined' && typeof query.replace !== 'undefined') {
+  if (typeof query.multiple !== 'undefined') {
+    for(var i = 0; i < query.multiple.length; i++){
+      var option = query.multiple[i];
+
+      source = source.replace(option.search, option.replace);
+    }
+  } else if (typeof query.search !== 'undefined' && typeof query.replace !== 'undefined') {
     if (typeof query.flags !== 'undefined') {
       query.search = new RegExp(query.search, query.flags);
     }
